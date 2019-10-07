2c6135285a41b02e4b283e91ccec4b7d46ec1b91
omar44336 <omarfaruk44336@gmail.com>
2019-10-08 01:36:23 +0600

    Validation Control

--- regforms/forms.py
+++ regforms/forms.py
@@ -1,8 +1,39 @@
 from django.contrib.auth.models import User
 from django import forms
-
+from .models import *
+from django.core.validators import validate_email
 class registrationform(forms.ModelForm):
-    class Meta:
+    username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter username'}),required=True,max_length=50)
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter email ID'}),
+                               required=True, max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter password'}),
+                               required=True, max_length=50)
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm password'}),
+                               required=True, max_length=50)
+    class Meta():
         model=User
-        fields=['username','email','password']
+        fields=['username','email','password','confirm_password']
 
+    def clean_username(self):
+        user = self.cleaned_data['username']
+        try:
+            match= User.objects.get(username = user)
+        except:
+            return self.cleaned_data['username']
+        raise forms.ValidationError("Username already exist.")
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if email and User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Email already exist.')
+        return email
+    def clean_confirm_password(self):
+        pas=self.cleaned_data.get('password')
+        cpas= self.cleaned_data.get('confirm_password')
+        MIN_LENGTH=8
+        if pas and cpas and pas != cpas:
+                 raise forms.ValidationError('password does not match')
+        else:
+            if len(pas) < MIN_LENGTH:
+                raise forms.ValidationError('Password should at least %d characters and Password should not all numeric ' %MIN_LENGTH)
+            if pas.isdigit():
+                raise forms.ValidationError('Password should not all numeric')
--- regforms/views.py
+++ regforms/views.py
@@ -1,46 +1,26 @@
-
-from django.shortcuts import render,redirect,HttpResponse,reverse,HttpResponsePermanentRedirect
-from .forms import registrationform
-from django.views.generic import View
-from django.contrib.auth import authenticate,login
-from django.urls import path
-
-from django.contrib import admin
-
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect,HttpResponse
 from django.contrib.auth.models import User
-
-
-
+from .forms import registrationform
 def signup(request):
-    form=registrationform(request.POST or None)
-    if request.method=='GET':
-        return render(request, 'regforms.html', {'form': form})
-    elif request.method=='POST':
-        form = registrationform(request.POST or None)
+    if request.method == 'POST':
+        form = registrationform(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get("username")
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = User.objects.create_user(username=username, email=email, password=password)
-            user.set_password(password)
-            user.is_active = True
+            username = form.cleaned_data.get('username')
+            email=form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password')
+            user=User.objects.create_user(username=username,email=email, password=raw_password)
+            user.set_password(raw_password)
+            user.is_active=True
             user.save()
-            user = authenticate(username=username, password=password, email=email)
+            user = authenticate(username=username, password=raw_password)
             if user is not None:
-             if user.is_active:
-                login(request, user)
-                return HttpResponsePermanentRedirect(reverse('UserProfile'))
-                # It is the power of name and namespase
-
+                if user.is_active:
+                    login(request,user)
+                    return redirect('UserProfile')
         else:
-            print(form.errors)
-            return render(request, 'regforms.html', {'form': form})
+            return render(request, 'regforms.html', {'frm': form})
 
-
-
-    else:return render(request, 'regforms.html', {'form': form})
-
-          # user authentication
-
-
-
+    else:
+        form = registrationform()
+    return render(request,'regforms.html', {'frm': form})
